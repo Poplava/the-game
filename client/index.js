8e33dde6d0d22f9f49680337d63721520c3dd631
Ihor Poplavskyi <ihor.poplavskyi@booking.com>
2017-07-10 10:19:30 +0200

    basic react setup

--- client/index.js
+++ client/index.js
@@ -1,3 +1,7 @@
-import _ from 'lodash';
+import React from 'react';
+import { render } from 'react-dom';
 
-console.log(_);
+render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
+import { join } from 'path';
+
 import express from 'express';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+
+import webpackConfig from '../webpack.config';
 
 const app = express();
 
+app.use(
+  webpackMiddleware(webpack(webpackConfig), {
+    publicPath: '/assets/'
+  })
+);
+
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
 app.listen(3000, () => console.log('Started...'));
